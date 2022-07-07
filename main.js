af459590dfd99ffcc1e2bba87c154ca7105fb636
mitch <mitchellanderson202@hotmail.com>
2022-07-08 09:12:34 +1000

    initial build

---
+++ main.js
@@ -0,0 +1,35 @@
+// Photo Slider
+var i = 0;
+var images = [];
+var time = 2000;
+
+images[0] = "./images/image0.jpg";
+images[1] = "./images/image1.jpg";
+images[2] = "./images/image2.jpg";
+images[3] = "./images/image3.jpg";
+
+function changeImg(){
+  document.slider.src = images[i];
+
+  if(i < images.length - 1){
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg();
+
+
+// Year
+document.getElementById("year").innerText = new Date().getFullYear();
+
+// Contact Button
+let contactBtn = document.getElementById("contact-btn");
+
+contactBtn.addEventListener("click", function() {
+  
+})
+
