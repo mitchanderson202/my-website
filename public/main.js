9b18edb4d0fc92f670a2f8b38d48ac6acfc8ee87
mitch <mitchellanderson202@hotmail.com>
2022-07-10 11:11:18 +1000

    posted live to firebase

--- public/main.js
+++ public/main.js
@@ -3,10 +3,10 @@ var i = 0;
 var images = [];
 var time = 2000;
 
-images[0] = "./images/image0.jpg";
-images[1] = "./images/image1.jpg";
-images[2] = "./images/image2.jpg";
-images[3] = "./images/image3.jpg";
+images[0] = "../images/image0.jpg";
+images[1] = "../images/image1.jpg";
+images[2] = "../images/image2.jpg";
+images[3] = "../images/image3.jpg";
 
 function changeImg(){
   document.slider.src = images[i];
