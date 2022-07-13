2c78c08c1cceea06ac1c967d68ab30a24d76c9dd
mitch <mitchellanderson202@hotmail.com>
2022-07-13 16:12:44 +1000

    updated image links

--- public/main.js
+++ public/main.js
@@ -1,14 +1,21 @@
 // Photo Slider
-var i = 0;
-var images = [];
-var time = 2000;
+let i = 0;
+let images = [];
+let time = 2000;
+let link = []
+
+let imageLink = document.getElementById("imageLink")
 
 images[0] = "./images/image0.jpg";
+link[0] = "https://c-e-n-e89d3.web.app/";
 images[1] = "./images/image1.jpg";
+link[1] = "https://api-fun-35f55.web.app/";
 images[2] = "./images/image2.jpg";
+link[2] = "https://rock-paper-scissors-4dffb.web.app/";
 
 function changeImg(){
   document.slider.src = images[i];
+  imageLink.href = link[i];
 
   if(i < images.length - 1){
     i++;
